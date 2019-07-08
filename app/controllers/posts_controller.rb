a26f0892b95d46bb67232eb9c624e64df447166a
svetlana berrios <sveta.berrios@gmail.com>
2019-07-08 18:56:39 +0300

    did whole lot, but not enough, clearly

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,8 @@
+class PostsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
+
+  def index
+    @posts = Post.all
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Post.create(
+  title: "Dance lessons",
+  text: "Hi, my name is Olga and i will teach you how to dance!"
+)
